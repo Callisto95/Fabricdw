720e24fa5e11ace22a7e3fd540f8380f102d70c7
UnknownUser95 <luluklzde@gmail.com>
2024-11-20 14:21:00 +0100

    clone properties instead of deleting them

--- fabricdw/properties/__init__.py
+++ fabricdw/properties/__init__.py
@@ -1,1 +1,8 @@
+from fabricdw.args import args
 from fabricdw.properties.modify import create_replacements, modify_properties
+
+
+def get_property(property_name: str, fallback: str = None) -> str | None:
+	if property_name in args().properties:
+		return args().properties[property_name]
+	return fallback
